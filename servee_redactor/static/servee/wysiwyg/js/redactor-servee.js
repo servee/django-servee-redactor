70654d2fbd0286504a2b16652dd7ad53dc1f7696
Kasey Kelly <kasey@servee.com>
2015-02-06 11:31:49 -0500

    plugins included

--- servee_redactor/static/servee/wysiwyg/js/redactor-servee.js
+++ servee_redactor/static/servee/wysiwyg/js/redactor-servee.js
@@ -1,8 +1,8 @@
 //@codekit-prepend "redactor.js";
-//@codekit-prepend "/plugins/table.js";
-//@codekit-prepend "/plugins/video.js";
-//@codekit-prepend "/plugins/imagemanager.js";
-//@codekit-prepend "/plugins/filemanager.js";
+//@codekit-prepend "plugins/table.js";
+//@codekit-prepend "plugins/video.js";
+//@codekit-prepend "plugins/imagemanager.js";
+//@codekit-prepend "plugins/filemanager.js";
 
 function load_wysiwyg($par){
     $par.find('textarea:not(.no_wysiwyg)').redactor({
