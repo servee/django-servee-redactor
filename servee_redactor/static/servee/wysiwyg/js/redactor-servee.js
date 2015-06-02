20b3207857003fa433bfb481f19572eee7bcfd94
Kasey Kelly <kasey@servee.com>
2015-06-02 12:57:04 -0400

    grumble

--- servee_redactor/static/servee/wysiwyg/js/redactor-servee.js
+++ servee_redactor/static/servee/wysiwyg/js/redactor-servee.js
@@ -40,7 +40,7 @@ function load_wysiwyg($par){
     });
 
 // init codemirror after redactor's call
-    var editor = CodeMirror.fromTextArea($par.find('textarea:not(.no_wysiwyg)'), {
+    var editor = CodeMirror.fromTextArea($par.find('textarea:not(.no_wysiwyg)')[0, 1, 2], {
         lineNumbers: true,
         mode: "text/html",
         matchBrackets: true
